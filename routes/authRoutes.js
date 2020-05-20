f5c9880ec5fa59b813a26a02757aec43e884e98b
Andrew Perez <andrewperez@andrews-air.home>
2020-05-20 15:42:49 -0700

    editing callback again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,7 +7,7 @@ module.exports = app =>{
         })
     )
     
-    app.get('/auth/google/callback',passport.authenticate('google'));
+    app.get('/auth/auth/google/callback',passport.authenticate('google'));
 
     app.get('/api/logout',(req,res)=>{
         req.logout();
