8db9f951af39734b3155ec412e4368b4976d6630
Andrew Perez <andrewperez@andrews-air.home>
2020-05-20 16:06:35 -0700

    consoling profile

--- services/passport.js
+++ services/passport.js
@@ -28,6 +28,7 @@ passport.use(new GoogleStrategy(
         callbackURL:'/auth/google/callback',
         proxy:true
      },(accessToken, refreshToken, profile,done)=>{
+         console.log(profile)
             User.findOne({googleId:profile.id})
                 .then(existingUser => {
                     if(existingUser){
